07315010bd41bfe1e5ca621d26671c4621eb96ea
JuanAlfaro777 <137421973+JuanAlfaro777@users.noreply.github.com>
2023-06-26 19:49:47 -0300

    BETA.py
    actualizacion del juego, se le agrego un menu antes de iniciar el juego y un " final del juego" al perder , mas el puntaje final de la misma partida y por ultimo con las opciones de salir del juego o reiniciar la partida( faltan modificaciones)
    
    Game update, a menu was added before starting the game and an "end of the game" when losing, plus the final score of the same game and finally with the options to exit the game or restart the game (more modifications are missing )

--- BETA.py
+++ BETA.py
@@ -1,4 +1,4 @@
-import random
+import random 
 import pygame
 from pygame.locals import *
 
@@ -17,9 +17,6 @@ background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
 # Dimensiones de la imagen del fondo
 background_height = background_image.get_height()
-
-# Posición inicial del fondo
-background_y = 0
 
 # Colores
 WHITE = (255, 255, 255)
@@ -59,7 +56,7 @@ class Meteor(pygame.sprite.Sprite):
         self.rect.x = random.randint(0, WIDTH - self.rect.width)
         self.rect.y = random.randint(-100, -40)
         self.speed_y = random.randint(2, 4)  # Ajusta el rango de velocidad
-        self.vida = 2  # Cantidad de veces que aparecerá el meteorito
+        self.vida = 4  # Cantidad de veces que aparecerá el meteorito
 
     def update(self):
         self.rect.y += self.speed_y
@@ -115,59 +112,136 @@ def render_score():
     score_text = font.render("Score: " + str(score), True, WHITE)
     window.blit(score_text, (50, 50))
 
-running = True
-while running:
-    dt = clock.tick(60) / 1000.0
-    score += int(dt * 30)  # Ajusta el factor de incremento según tus necesidades
-    for event in pygame.event.get():
-        if event.type == QUIT:
+# Función para mostrar el menú
+def show_menu():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return None
+            elif event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    return "start"
+                elif event.key == K_ESCAPE:
+                    pygame.quit()
+                    return None
+
+        # Dibujar el fondo
+        window.blit(background_image, (0, 0))
+
+        # Renderizar el título y las opciones del menú
+        title_font = pygame.font.Font(None, 64)
+        title_text = title_font.render("Esquiva los meteoritos", True, WHITE)
+        window.blit(title_text, (WIDTH // 2 - title_text.get_width() // 2, 200))
+
+        menu_font = pygame.font.Font(None, 36)
+        start_text = menu_font.render("Presiona ESPACIO para comenzar", True, WHITE)
+        quit_text = menu_font.render("Presiona ESC para salir", True, WHITE)
+        window.blit(start_text, (WIDTH // 2 - start_text.get_width() // 2, 300))
+        window.blit(quit_text, (WIDTH // 2 - quit_text.get_width() // 2, 350))
+
+        pygame.display.flip()
+
+# Función para el bucle principal del juego
+def game_loop():
+    global score, background_y
+
+    # Reiniciar variables
+    score = 0
+    background_y = 0
+    game_started = True
+
+    running = True
+    while running:
+        dt = clock.tick(60) / 1000.0
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    spaceship.speed_x = -5
+                elif event.key == K_RIGHT:
+                    spaceship.speed_x = 5
+                elif event.key == K_UP:
+                    spaceship.speed_y = -5
+                elif event.key == K_DOWN:
+                    spaceship.speed_y = 5
+            elif event.type == KEYUP:
+                if event.key == K_LEFT or event.key == K_RIGHT:
+                    spaceship.speed_x = 0
+                elif event.key == K_UP or event.key == K_DOWN:
+                    spaceship.speed_y = 0
+
+        # Actualizar solo si el juego ha comenzado
+        if game_started:
+            score += int(dt * 60)  # Ajusta el factor de incremento según tus necesidades
+
+        # Actualizar
+        all_sprites.update()
+
+        # Mover el fondo de forma infinita
+        background_y += 1
+        if background_y >= background_height:
+            background_y = 0
+
+        # Dibujar el fondo
+        window.blit(background_image, (0, background_y))
+        window.blit(background_image, (0, background_y - background_height))
+
+        # Dibujar todos los sprites
+        all_sprites.draw(window)
+
+        # Verificar colisión entre la nave espacial y los meteoritos
+        collisions = pygame.sprite.spritecollide(spaceship, meteors, False)
+        if collisions:
             running = False
-        elif event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                spaceship.speed_x = -5
-            elif event.key == K_RIGHT:
-                spaceship.speed_x = 5
-            elif event.key == K_UP:
-                spaceship.speed_y = -5
-            elif event.key == K_DOWN:
-                spaceship.speed_y = 5
-        elif event.type == KEYUP:
-            if event.key == K_LEFT or event.key == K_RIGHT:
-                spaceship.speed_x = 0
-            elif event.key == K_UP or event.key == K_DOWN:
-                spaceship.speed_y = 0
 
-    # Actualizar
-    all_sprites.update()
+        # Renderizar el puntaje en pantalla
+        render_score()
 
-    # Mover el fondo de forma infinita
-    background_y += 1
-    if background_y >= background_height:
-        background_y = 0
+        pygame.display.flip()
+# Mostrar la pantalla de fin de juego
+    show_game_over()
 
-    # Dibujar el fondo
-    window.blit(background_image, (0, background_y))
-    window.blit(background_image, (0, background_y - background_height))
+# Función para mostrar la pantalla de fin de juego
+def show_game_over():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return None
+            elif event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    return "start"
+                elif event.key == K_ESCAPE:
+                    pygame.quit()
+                    return None
 
-    # Colisiones
-    if pygame.sprite.spritecollide(spaceship, meteors, False):
-        running = False
+        # Dibujar el fondo
+        window.blit(background_image, (0, 0))
 
-    # Eliminar meteoritos que hayan agotado su vida
-    for meteor in meteors.copy():
-        if meteor.vida <= 0:
-            meteor.kill()
+        # Renderizar el mensaje de fin de juego
+        game_over_font = pygame.font.Font(None, 64)
+        game_over_text = game_over_font.render("¡Fin del juego!", True, WHITE)
+        window.blit(game_over_text, (WIDTH // 2 - game_over_text.get_width() // 2, 200))
 
-    # Generar nuevos meteoritos si es necesario
-    while len(meteors) < 8:
-        meteor_type = random.randint(1, 4)
-        meteor = Meteor(meteor_type)
-        all_sprites.add(meteor)
-        meteors.add(meteor)
+        score_font = pygame.font.Font(None, 36)
+        score_text = score_font.render("Puntaje final: " + str(score), True, WHITE)
+        window.blit(score_text, (WIDTH // 2 - score_text.get_width() // 2, 300))
 
-    # Renderizar
-    all_sprites.draw(window)
-    render_score()
-    pygame.display.flip()
+        restart_text = score_font.render("Presiona ESPACIO para reiniciar", True, WHITE)
+        quit_text = score_font.render("Presiona ESC para salir", True, WHITE)
+        window.blit(restart_text, (WIDTH // 2 - restart_text.get_width() // 2, 400))
+        window.blit(quit_text, (WIDTH // 2 - quit_text.get_width() // 2, 450))
 
+        pygame.display.flip()
+        
+# Bucle principal del programa
+while True:
+    choice = show_menu()
+    if choice == "start":
+        game_loop()
+    else:
+        break
 pygame.quit()
